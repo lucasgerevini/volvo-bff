b679923fa520ebac94bfe39ac76df20e81ff0cc6
Lucas Gerevini <lucasgerevini@outlook.com>
2021-01-08 08:12:45 -0300

    automigration

--- Volvo.BFF/Startup.cs
+++ Volvo.BFF/Startup.cs
@@ -57,8 +57,10 @@ namespace Volvo.BFF
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, VolvoContext volvoContext)
         {
+            volvoContext.Database.Migrate();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
